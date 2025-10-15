969e028c54dff800d35740b72926497d8626b2ac
abdeljalilelouafi <abdeljalilelouafi2@gmail.com>
2025-10-15 14:06:23 +0100

    DH-4 Initializing the doctor crud

---
+++ src/main/java/com/digitalhospital/dao/ConsultationDAO.java
@@ -0,0 +1,50 @@
+package com.digitalhospital.dao;
+
+import com.digitalhospital.model.Consultation;
+import com.digitalhospital.util.JPAUtil;
+import jakarta.persistence.EntityManager;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+public class ConsultationDAO extends GenericDAO<Consultation> {
+    public ConsultationDAO() {
+        super(Consultation.class);
+    }
+
+    public List<Consultation> findByPatientId(Long idPatient) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            return em.createQuery("SELECT c FROM Consultation c WHERE c.patient.idPersonne = :id", Consultation.class)
+                    .setParameter("id", idPatient)
+                    .getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Consultation> findByDocteurId(Long idDocteur) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            return em.createQuery("SELECT c FROM Consultation c WHERE c.patient.idPersonne = :id", Consultation.class)
+                    .setParameter("id", idDocteur)
+                    .getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public boolean isCreneauOccupe(LocalDate date, LocalTime heure) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            Long count = em.createQuery(
+                            "SELECT COUNT(c) FROM Consultation c WHERE c.date = :date AND c.heure = :heure", Long.class)
+                    .setParameter("date", date)
+                    .setParameter("heure", heure)
+                    .getSingleResult();
+            return count > 0;
+        } finally {
+            em.close();
+        }
+    }
+}
